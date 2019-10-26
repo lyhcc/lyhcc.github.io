1c136e4f439578707c6f882b07245139ae301f5c
lyhcc <lyhcc_email@163.com>
2019-10-26 20:08:31 +0800

    Site updated: 2019-10-26 20:08:10

--- js/gallery.js
+++ js/gallery.js
@@ -1,19 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     if (typeof ($.fn.lightGallery) === 'function') {
-        $('.article').lightGallery({ selector: '.gallery-item'});
+        $('.article').lightGallery({ selector: '.gallery-item' });
     }
     if (typeof ($.fn.justifiedGallery) === 'function') {
         if ($('.justified-gallery > p > .gallery-item').length) {
             $('.justified-gallery > p > .gallery-item').unwrap();
         }
-        $('.justified-gallery').justifiedGallery({rowHeight:160, margins:4,
-            thumbnailPath: function (currentPath, width, height, image) {
-                if (Math.max(width, height) < 400) {
-                    return currentPath;
-                } else { 
-                    return currentPath + "!photo_thumb";
-                }
-              }
-        });
+        $('.justified-gallery').justifiedGallery();
     }
 });
