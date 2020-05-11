6b82ffbd2b40c7562c9a7382f287a79d6ecd1532
lyhcc <lyhcc_email@163.com>
2020-05-11 11:39:54 +0800

    Site updated: 2020-05-11 11:39:28

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
-(function ($) {
-    $('.article img:not(".not-gallery-item")').each(function () {
+(function($) {
+    $('.article img:not(".not-gallery-item")').each(function() {
         // wrap images with link and add caption if possible
         if ($(this).parent('a').length === 0) {
-            $(this).wrap('<a class="gallery-item" href="' + $(this).attr('src') + '"></a>');
+            // $(this).wrap('<a class="gallery-item" href="' + $(this).attr('src') + '"></a>');
+            $(this).wrap('<a class="gallery-item" style="display:block;text-align:center;" href="' + $(this).attr('src') + '"></a>');
             if (this.alt) {
                 $(this).after('<div class="has-text-centered is-size-6 has-text-grey caption">' + this.alt + '</div>');
             }
         }
     });
 
-    if (typeof (moment) === 'function') {
-        $('.article-meta time').each(function () {
+    if (typeof(moment) === 'function') {
+        $('.article-meta time').each(function() {
             $(this).text(moment($(this).attr('datetime')).fromNow());
         });
     }
 
-    $('.article > .content > table').each(function () {
+    $('.article > .content > table').each(function() {
         if ($(this).width() > $(this).parent().width()) {
             $(this).wrap('<div class="table-overflow"></div>');
         }
@@ -33,11 +34,11 @@
     $(window).resize(adjustNavbar);
 
     $('figure.highlight table').wrap('<div class="highlight-body">');
-    if (typeof (IcarusThemeSettings) !== 'undefined' &&
-        typeof (IcarusThemeSettings.article) !== 'undefined' &&
-        typeof (IcarusThemeSettings.article.highlight) !== 'undefined') {
-        if (typeof (ClipboardJS) !== 'undefined' && IcarusThemeSettings.article.highlight.clipboard) {
-            $('figure.highlight').each(function () {
+    if (typeof(IcarusThemeSettings) !== 'undefined' &&
+        typeof(IcarusThemeSettings.article) !== 'undefined' &&
+        typeof(IcarusThemeSettings.article.highlight) !== 'undefined') {
+        if (typeof(ClipboardJS) !== 'undefined' && IcarusThemeSettings.article.highlight.clipboard) {
+            $('figure.highlight').each(function() {
                 var id = 'code-' + Date.now() + (Math.random() * 1000 | 0);
                 var button = '<a href="javascript:;" class="copy" title="Copy" data-clipboard-target="#' + id + ' .code"><i class="fas fa-copy"></i></a>';
                 $(this).attr('id', id);
@@ -52,7 +53,7 @@
         var fold = IcarusThemeSettings.article.highlight.fold;
         if (fold.trim()) {
             var button = '<span class="fold">' + (fold === 'unfolded' ? '<i class="fas fa-angle-down"></i>' : '<i class="fas fa-angle-right"></i>') + '</span>';
-            $('figure.highlight').each(function () {
+            $('figure.highlight').each(function() {
                 if ($(this).find('figcaption').length) {
                     $(this).find('figcaption').prepend(button);
                 } else {
@@ -67,10 +68,10 @@
                 !isFolded ? $toggle.addClass('fa-angle-down') : $toggle.addClass('fa-angle-right');
             }
 
-            $('figure.highlight').each(function () {
+            $('figure.highlight').each(function() {
                 toggleFold(this, fold === 'folded');
             });
-            $('figure.highlight figcaption .fold').click(function () {
+            $('figure.highlight figcaption .fold').click(function() {
                 var $code = $(this).closest('figure.highlight');
                 toggleFold($code.eq(0), !$code.hasClass('folded'));
             });
